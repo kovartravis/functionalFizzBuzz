a0099f7de5e6281efe285c4df9cf62f005be5ff0
unknown <kovartravis@gmail.com>
2017-10-25 08:50:22 -0500

    removed print function and used functional version instead

--- functionalFizzBuzz.js
+++ functionalFizzBuzz.js
@@ -1,14 +1,8 @@
 const _ = require('lodash')
 
-const printPretty = (a) =>{
-    for(i = 0; i < a.length; i++){
-        console.log(a[i])
-    }
-}
-
 let array = _.map(_.range(1, 100), a => {if(a%15 == 0){return a + " :FizzBuzz"} else return a})
 array = _.map(array, a => {if(a%3 == 0){return a + " :Fizz"} else return a})
 array = _.map(array, a => {if(a%5 == 0){return a + " :Buzz"} else return a})
 array = _.filter(array, a => typeof a !== 'number')
 
-printPretty(array)
+_.forEach(array, (a) => console.log(a));
